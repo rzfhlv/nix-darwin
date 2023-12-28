60202c55fba43483944b5889c2413429a6296b8b
Reza Fahlevi <rzfhlv.me@gmail.com>
2023-12-28 20:40:36 +0700

    takeout add new line

--- home.nix
+++ home.nix
@@ -38,7 +38,7 @@
     # custom settings
     enableFishIntegration = true;
     settings = {
-      add_newline = true;
+      # add_newline = true;
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = false;
